7046a4925dca37ed6c9952443b4b12f9f9720f43
Gloria Edem <gloedem@gmail.com>
2021-07-30 21:48:08 +0000

    cleanup

--- index.js
+++ index.js
@@ -52,31 +52,19 @@ class Deposit extends Transaction {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 const myAccount = new Account("itsBritneyBetch");
-//console.log("Initial balance:", myAccount.balance);
-//console.log(myAccount);
-//const t1 = new Deposit(120.0, myAccount);
-//console.log(t1.commit());
-////console.log("Transaction 1:", t1);
-
-//// const t2 = new Withdrawal(15.99, myAccount);
-//// t2.commit();
-//// console.log("Transaction 2:", t2);
-//// console.log(myAccount);
-//console.log("Final balance:", myAccount.balance);
-console.log("Starting Account Balance: ", myAccount.balance);
-
-console.log("Attempting to withdraw should fail...");
-const t1 = new Withdrawal(100.0, myAccount);
+console.log("Initial balance:", myAccount.balance);
+console.log(myAccount);
+const t1 = new Deposit(120000.0, myAccount);
 console.log("Commit result:", t1.commit());
 console.log("Account Balance: ", myAccount.balance);
 
-console.log("Depositing some paycheck");
+console.log("More money, less problems - deposit that paycheck!");
 const t2 = new Deposit(74500.0, myAccount);
 console.log("Commit result:", t2.commit());
 console.log("Account Balance: ", myAccount.balance);
 
-console.log("Withdrawal - gotta buy a car!");
-const t3 = new Withdrawal(25999.0, myAccount);
+console.log("Withdrawal - gotta buy a Tesla!");
+const t3 = new Withdrawal(65999.0, myAccount);
 console.log("Commit result:", t3.commit());
 
 console.log("Ending Account Balance: ", myAccount.balance);
